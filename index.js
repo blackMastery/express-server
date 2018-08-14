ab94baf332c65ef77b153d45377bf97b7ec812ee
blackMastery <black.king1232@gmail.com>
2018-08-14 19:12:56 -0400

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var app = require('./server');
 var config = require('./config/config');
 const path = require('path')
-
+const WebSocket = require('ws');
 const {port} = config.dev;
 
+
+
+
+
+// const socket = new WebSocket.Server({port:6060})
+
+// socket.on('connection', function connection(w_socket) {
+// 	w_socket.on('message',function incomming(message) {
+// 		console.log('received: %s', message)
+// 	})
+
+// 	w_socket.send("hello from WebSocket server")
+// })
 
 
 
--- models/user.js
+++ models/user.js
@@ -1,3 +1,7 @@
+/* New requirements: fields for the user models 
+User attribute : profilePicURl, firstname lastname email passsword date 
+post reference, description, age, field_of_study, 
+*/
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -10,7 +14,10 @@ const UserSchema = new Schema({
 	email: {type:String, required:true, unique: true},
 	password: {type: String, required:true},
     date :{ type: Date, default: Date.now },
-    post :[{type: Schema.ObjectId, ref: "Post"}]
+    post :[{type: Schema.ObjectId, ref: "Post"}],
+    description: {type:String,required:false},
+    age:{type:Number, required:false},
+    field_of_study:{type:String, required:false}
 	
 })
 
